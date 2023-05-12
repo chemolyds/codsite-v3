0bcfd97ea93228e84d34b48d32758b532ce116fe
Jerdan1980 <jeremytinana@gmail.com>
2023-05-12 00:05:03 -0400

    Separate filtering by tier

--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,13 +1,23 @@
 import Layout from '@/components/layout'
-import { ResourceList, TagOption, TagOptions } from '@/lib/resources'
+import {
+  Resource,
+  ResourceList,
+  TagOption,
+  TagOptions,
+  ResourceTags,
+} from '@/lib/resources'
 import {
   Card,
   CardBody,
   CardHeader,
+  Center,
+  Checkbox,
+  CheckboxGroup,
   Grid,
   GridItem,
   Heading,
   LinkOverlay,
+  Stack,
   Tag,
   Wrap,
 } from '@chakra-ui/react'
@@ -18,6 +28,23 @@ import { useState } from 'react'
 
 export default function Resources() {
   const [selectedTags, setSelectedTags] = useState<MultiValue<TagOption>>([])
+  const [selectedTiers, setSelectedTiers] = useState<ResourceTags[]>([
+    'Bronze',
+    'Silver',
+    'Gold',
+  ])
+
+  function filter(resource: Resource) {
+    // Filter out tiers first
+    if (!selectedTiers.some((tier) => resource.tags?.includes(tier)))
+      return false
+
+    // Then filter by tag
+    return (
+      selectedTags.length === 0 ||
+      selectedTags.some((tagOption) => resource.tags?.includes(tagOption.value))
+    )
+  }
 
   return (
     <Layout
@@ -27,6 +54,21 @@ export default function Resources() {
       <Heading as="h1" textAlign="center" mb="10">
         Resources
       </Heading>
+
+      <Center my="3">
+        <CheckboxGroup
+          colorScheme="brand"
+          size="lg"
+          defaultValue={selectedTiers}
+          onChange={(tiers: ResourceTags[]) => setSelectedTiers(tiers)}
+        >
+          <Stack spacing={[1, 5]} direction={['column', 'row']}>
+            <Checkbox value="Bronze">Bronze</Checkbox>
+            <Checkbox value="Silver">Silver</Checkbox>
+            <Checkbox value="Gold">Gold</Checkbox>
+          </Stack>
+        </CheckboxGroup>
+      </Center>
 
       <Select<TagOption, true, GroupBase<TagOption>>
         isMulti
@@ -38,36 +80,32 @@ export default function Resources() {
 
       <Grid templateColumns="repeat(2, 1fr)" gap={8} mt="10">
         <AnimatePresence>
-          {ResourceList.filter(
-            (resource) =>
-              selectedTags.length === 0 ||
-              selectedTags.some((tagOption) =>
-                resource.tags?.includes(tagOption.value)
-              )
-          ).map((resource) => (
-            <GridItem
-              key={resource.link}
-              as={motion.div}
-              layout
-              layoutId={resource.link}
-            >
-              <Card>
-                <CardHeader>
-                  <Heading size="xs" pb="2">
-                    <LinkOverlay href={resource.link} as={Link} isExternal>
-                      {resource.name}
-                    </LinkOverlay>
-                  </Heading>
-                  <Wrap>
-                    {resource.tags.map((tag) => (
-                      <Tag key={tag}>{tag}</Tag>
-                    ))}
-                  </Wrap>
-                </CardHeader>
-                <CardBody pt="0">{resource.description}</CardBody>
-              </Card>
-            </GridItem>
-          ))}
+          {ResourceList.filter((resource) => filter(resource)).map(
+            (resource) => (
+              <GridItem
+                key={resource.link}
+                as={motion.div}
+                layout
+                layoutId={resource.link}
+              >
+                <Card>
+                  <CardHeader>
+                    <Heading size="xs" pb="2">
+                      <LinkOverlay href={resource.link} as={Link} isExternal>
+                        {resource.name}
+                      </LinkOverlay>
+                    </Heading>
+                    <Wrap>
+                      {resource.tags.map((tag) => (
+                        <Tag key={tag}>{tag}</Tag>
+                      ))}
+                    </Wrap>
+                  </CardHeader>
+                  <CardBody pt="0">{resource.description}</CardBody>
+                </Card>
+              </GridItem>
+            )
+          )}
         </AnimatePresence>
       </Grid>
     </Layout>
