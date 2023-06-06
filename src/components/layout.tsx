40a41c4aafb901aa5b325cadbce6bf5d6eddcba0
Jerdan1980 <jeremytinana@gmail.com>
2023-06-05 22:38:45 -0400

    Fix scrolling on firefox

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Heading } from '@chakra-ui/react'
 import Head from 'next/head'
 import NavBar from './navbar'
 import { motion, useReducedMotion } from 'framer-motion'
@@ -15,19 +15,21 @@ export default function Layout({
   const shouldReduceMotion = useReducedMotion()
 
   return (
-    <Box maxH="calc(100vh)" overflow="clip">
+    <Box /*maxH="calc(100vh)" overflow="clip"*/>
       <Head>
         <title>{`CODsite | ${title}`}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar />
+      {/* https://dev.to/shriram27/fixed-navbar-using-chakra-ui-4i7b */}
+      <NavBar pos="fixed" w="full" zIndex="200" />
       {/* https://javascript.plainenglish.io/how-to-animate-the-page-transition-in-next-js-68c7b888dce3 */}
       {/* https://letsbuildui.dev/articles/animated-page-transitions-in-nextjs */}
       <Box
-        overflowY="auto"
-        maxHeight="87vh"
+        /*overflowY="auto"
+        maxHeight="87vh"*/
+        mt={24}
         as={motion.div}
         key={title}
         initial="initial"
