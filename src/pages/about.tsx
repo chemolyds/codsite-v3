d2de803b56bad0e56475c83480e36c288c81a7c2
Jerdan1980 <jeremytinana@gmail.com>
2023-06-12 23:22:53 -0400

    Switch png to jpg

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,10 +17,10 @@ import {
 import Image from 'next/image'
 
 import dillionImg from '@/images/staff/dillion.jpeg'
-import elizaImg from '@/images/staff/eliza 2x.png'
+import elizaImg from '@/images/staff/eliza 2x.jpg'
 import ihorImg from '@/images/staff/ihor.jpg'
-import jeremyImg from '@/images/staff/jeremy 2x.png'
-import kwanwooImg from '@/images/staff/kwanwoo 2x.png'
+import jeremyImg from '@/images/staff/jeremy 2x.jpg'
+import kwanwooImg from '@/images/staff/kwanwoo 2x.jpg'
 import nicolaiImg from '@/images/staff/nicolai.jpg'
 
 export default function About() {
