4d7b83bbf0085b5f1e9d3b6ae33e69ed0a362140
EADChemistry <eadchemistry@gmail.com>
2023-06-17 17:33:14 -0400

    Update competitions

--- src/lib/resources.ts
+++ src/lib/resources.ts
@@ -50,13 +50,13 @@ export const ResourceList: Resource[] = [
   },
   {
     name: 'CODSNotes: Organic Part I',
-    description: 'Notes on Clayden Organic Chemistry, by CODSstaff',
+    description: 'Notes on Clayden Organic Chemistry, by CODStaff',
     link: 'https://docs.google.com/document/d/e/2PACX-1vRW1bZc-38iNT3fPRcQslL-pA1GNlpO9UifFA2J7T1HOpDPiqcSVWV2tFJu-PQYHQa6SmARu_fWNicG/pub',
     tags: ['Silver', 'Gold', 'Organic'],
   },
   {
     name: 'CODSNotes: Organic Part II',
-    description: 'Notes on Clayden Organic Chemistyr, by CODStaff',
+    description: 'Notes on Clayden Organic Chemistry, by CODStaff',
     link: 'https://docdro.id/QKJYHub',
     tags: ['Silver', 'Gold', 'Organic'],
   },
@@ -86,7 +86,7 @@ export const ResourceList: Resource[] = [
     name: 'Organic Chemistry Problems',
     description: 'Huge collection of organic chemistry problems',
     link: 'https://www.organicchemproblems.com',
-    tags: ['Bronze', 'Silver', 'Gold', 'Organic', 'Problem Set'],
+    tags: ['Gold', 'Organic', 'Problem Set'],
   },
   {
     name: 'Evans Advanced Organic Chemistry Notes and Problems',
@@ -111,7 +111,7 @@ export const ResourceList: Resource[] = [
     name: 'Problem of the Day Archive',
     description: 'Compilation of every CODS Original POTD',
     link: 'https://docs.google.com/document/d/1LxA8xap-O20o3zdQSDub9lRHUPocsjO18QxIhbe6vcE/edit?usp=sharing',
-    tags: ['Silver', 'Gold', 'General', 'Physical', 'Organic', 'Inorganic'],
+    tags: ['Silver', 'Gold', 'General', 'Physical', 'Organic', 'Inorganic', 'Problem Set'],
   },
   {
     name: 'USNCO Exams',
@@ -141,13 +141,13 @@ export const ResourceList: Resource[] = [
     name: 'Master Organic Chemistry',
     description: 'Free summary sheets on organic chemistry',
     link: 'https://www.masterorganicchemistry.com/summary-sheets/',
-    tags: ['Bronze', 'Organic'],
+    tags: ['Silver', 'Organic'],
   },
   {
     name: 'Richard Thornley',
     description: 'A funner way to learn A-level/IB chemistry',
     link: 'https://www.youtube.com/user/richthornley/featured',
-    tags: ['Bronze', 'Silver', 'General', 'Analytical', 'YouTube'],
+    tags: ['Bronze', 'General', 'Analytical', 'YouTube'],
   },
   {
     name: 'Scott Milam',
@@ -158,8 +158,8 @@ export const ResourceList: Resource[] = [
   {
     name: 'Organic Chemistry Tutor',
     description: 'Organic Chemistry Concept Explanations',
-    link: 'https://www.youtube.com/c/TheOrganicChemistryTutor/featured',
-    tags: ['Bronze', 'Silver', 'Organic'],
+    link: 'https://youtube.com/playlist?list=PL0o_zxa4K1BVWq4vvQWwdb-hThAoIkBiR',
+    tags: ['Silver', 'Organic'],
   },
   {
     name: 'CODS Youtube',
@@ -196,5 +196,35 @@ export const ResourceList: Resource[] = [
     description: 'Easy to understand IR spectroscopy sheet.',
     link: 'https://www.sigmaaldrich.com/US/en/technical-documents/technical-article/analytical-chemistry/photometry-and-reflectometry/ir-spectrum-table',
     tags: ['Silver', 'Gold', 'Analytical'],
+  },
+  {
+    name: 'Allery Chemistry',
+    description: 'Introductory chemistry explained simply',
+    link: 'https://www.youtube.com/@AlleryChemistry/videos',
+    tags: ['Bronze', 'General']
+  },
+  {
+    name: 'Zumdahl Chemistry',
+    description: 'Recommended beginner  general chemistry textbook',
+    link: 'https://www.amazon.com/s?k=zumdahl+chemistry&i=stripbooks&crid=3W0FQB4GGS4EN&sprefix=zumdahl+chemistry%2Cstripbooks%2C116&ref=nb_sb_noss_2',
+    tags: ['Bronze', 'General', 'Physical', 'Textbook']
+  },
+  {
+    name: 'Atkins Chemical Principles: A Quest for Insight',
+    description: 'Recommended non-beginner general chemistry textbook',
+    link: 'https://www.amazon.com/s?k=atkins+chemical+principles&i=stripbooks&crid=UO9AAW27T69Y&sprefix=atkins+chemical+principles%2Cstripbooks%2C126&ref=nb_sb_noss_1',
+    tags: ['Bronze', 'Silver', 'General', 'Physical', 'Inorganic', 'Textbook']
+  },
+  {
+    name: 'Klein Organic Chemistry',
+    description: 'Recommended beginner organic chemistry textbook',
+    link: 'https://www.amazon.com/s?k=%27Organic+Chemistry%27+Textbook+by+David+Klein+4th+Edition&i=stripbooks&crid=E0ICJWCOK5CZ&sprefix=organic+chemistry%27+textbook+by+david+klein+4th+edition%2Cstripbooks%2C106&ref=nb_sb_noss',
+    tags: ['Silver', 'Organic', 'Textbook']
+  },
+  {
+    name: 'Clayden Organic Chemistry',
+    description: 'Recommended non-beginner organic chemistry textbook',
+    link: 'https://www.amazon.com/s?k=Organic+Chemistry+Clayden&i=stripbooks&crid=53PE1VMQGDYI&sprefix=organic+chemistry+clayden%2Cstripbooks%2C99&ref=nb_sb_noss_1',
+    tags: ['Silver', 'Gold', 'Organic', 'Textbook']
   }
 ]
