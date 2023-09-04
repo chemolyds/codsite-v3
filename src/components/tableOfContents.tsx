6f4afed731c02f7f5f620c5398b230037a43acd5
Jerdan1980 <jeremytinana@gmail.com>
2023-09-04 16:27:14 -0400

    ToC checks if contents are null

--- src/components/tableOfContents.tsx
+++ src/components/tableOfContents.tsx
@@ -1,8 +1,7 @@
-import { 
-  Box, 
-  ListItem, 
-  UnorderedList, 
-  Heading,
+import {
+  Box,
+  ListItem,
+  UnorderedList,
   useColorModeValue,
 } from '@chakra-ui/react'
 import ScrollLink from './scrollLink'
@@ -11,34 +10,31 @@ export default function tableOfContents({
   contents,
 }: {
   contents: {
-    title: string,
-    id: string,
+    title: string
+    id: string
   }[]
 }) {
   return (
-    <Box 
-      display={{ base: "none", lg: "block"}} 
-      position="sticky" 
-      maxHeight="200px" 
+    <Box
+      display={{ base: 'none', lg: 'block' }}
+      position="sticky"
+      maxHeight="200px"
       top="120px"
       width="230px"
     >
-      <Box 
+      <Box
         px={6}
         py={5}
-        borderRadius='lg' 
+        borderRadius="lg"
         bg={useColorModeValue('white', 'gray.800')}
       >
-        <h4 style={{marginTop: "0"}}>Table of Contents</h4>
-        <UnorderedList 
-          ml={0}
-          style={{ padding: "0" }}
-        >
-          {contents.map(({title, id}) => (
-            <ListItem 
-              key={id} 
-              listStyleType="none" 
-              style={{ margin: "0", padding: "10px 0" }}
+        <h4 style={{ marginTop: '0' }}>Table of Contents</h4>
+        <UnorderedList ml={0} style={{ padding: '0' }}>
+          {contents?.map(({ title, id }) => (
+            <ListItem
+              key={id}
+              listStyleType="none"
+              style={{ margin: '0', padding: '10px 0' }}
             >
               <ScrollLink id={id} offset={100}>
                 {title}
