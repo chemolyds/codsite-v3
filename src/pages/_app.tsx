53947f81b5a69c58b59335c03072754a0b8e729d
Jerdan1980 <jeremytinana@gmail.com>
2025-06-19 14:30:50 -0400

    Updated provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
-import theme from '@/styles/theme'
-import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
 //import '@fontsource/exo-2/400.css'
 import '@fontsource/nunito/400.css'
 //import '@fontsource/open-sans/700.css'
+import { Provider } from '@/components/ui/provider'
 import '@fontsource/raleway/variable.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <Provider>
       <Component {...pageProps} />
-    </ChakraProvider>
+    </Provider>
   )
 }
