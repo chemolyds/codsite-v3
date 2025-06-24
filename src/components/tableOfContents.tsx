abd0ef9b6121b5e2593f7830286a011f8ebcb93c
Jerdan1980 <jeremytinana@gmail.com>
2025-06-24 07:53:22 -0400

    Update ToC (WIP)

--- src/components/tableOfContents.tsx
+++ src/components/tableOfContents.tsx
@@ -1,12 +1,8 @@
-import {
-  Box,
-  ListItem,
-  UnorderedList,
-  useColorModeValue,
-} from '@chakra-ui/react'
+import { Box, List } from '@chakra-ui/react'
 import ScrollLink from './scrollLink'
+import { useColorModeValue } from './ui/color-mode'
 
-export default function tableOfContents({
+export default function TableOfContents({
   contents,
 }: {
   contents: {
@@ -29,9 +25,9 @@ export default function tableOfContents({
         bg={useColorModeValue('white', 'gray.800')}
       >
         <h4 style={{ marginTop: '0' }}>Table of Contents</h4>
-        <UnorderedList ml={0} style={{ padding: '0' }}>
+        <List.Root ml={0} style={{ padding: '0' }}>
           {contents?.map(({ title, id }) => (
-            <ListItem
+            <List.Item
               key={id}
               listStyleType="none"
               style={{ margin: '0', padding: '10px 0' }}
@@ -39,9 +35,9 @@ export default function tableOfContents({
               <ScrollLink id={id} offset={100}>
                 {title}
               </ScrollLink>
-            </ListItem>
+            </List.Item>
           ))}
-        </UnorderedList>
+        </List.Root>
       </Box>
     </Box>
   )
